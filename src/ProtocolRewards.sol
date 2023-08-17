1d8e9ed21bf92eba9d2ea6c825fe82a7144d20ab
Wayne <W@WayneHoover.com>
2023-08-16 15:04:26 -1000

    add balance functions

--- src/ProtocolRewards.sol
+++ src/ProtocolRewards.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {Enjoy} from "../_imagine/Enjoy.sol";
 import {EIP712} from "./lib/EIP712.sol";
 import {IProtocolRewards} from "./interfaces/IProtocolRewards.sol";
 
@@ -16,6 +15,9 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
 
     /// @notice An account's nonce for gasless withdraws
     mapping(address => uint256) public nonces;
+
+    /// @notice Total Balance across all accounts
+    uint256 public totalBalance;
 
     constructor() payable EIP712("ProtocolRewards", "1") {}
 
@@ -33,9 +35,30 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
             revert ADDRESS_ZERO();
         }
 
-        balanceOf[to] += msg.value;
+        _increaseBalance(to, msg.value);
 
         emit Deposit(msg.sender, to, reason, msg.value, comment);
+    }
+
+    /// @notice Allow admin to increase balance of an address only up the amount of excess ETH in the contract
+    /// @dev TODO we will probably need a batch version of this function
+    /// @param to The address to increase the balance of
+    /// @param amount The amount to increase the balance by
+    function increaseBalance(address to, uint256 amount) external onlyOwner {
+        if (amount > address(this).balance - totalBalance) {
+            revert INVALID_AMOUNT();
+        }
+        _increaseBalance(to, amount);
+    }
+
+    function _increaseBalance(address to, uint256 amount) internal {
+        balanceOf[to] += amount;
+        totalBalance += amount;
+    }
+
+    function _decreaseBalance(address to, uint256 amount) internal {
+        balanceOf[to] -= amount;
+        totalBalance -= amount;
     }
 
     /// @notice Generic function to deposit ETH for multiple recipients, with an optional comment
@@ -75,7 +98,7 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
                 revert ADDRESS_ZERO();
             }
 
-            balanceOf[currentRecipient] += currentAmount;
+            _increaseBalance(currentRecipient, currentAmount);
 
             emit Deposit(msg.sender, currentRecipient, reasons[i], currentAmount, comment);
 
@@ -83,66 +106,6 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
                 ++i;
             }
         }
-    }
-
-    /// @notice Used by Zora ERC-721 & ERC-1155 contracts to deposit protocol rewards
-    /// @param creator Creator for NFT rewards
-    /// @param creatorReward Creator reward amount
-    /// @param createReferral Creator referral
-    /// @param createReferralReward Creator referral reward
-    /// @param mintReferral Mint referral user
-    /// @param mintReferralReward Mint referral amount
-    /// @param firstMinter First minter reward
-    /// @param firstMinterReward First minter reward amount
-    /// @param zora ZORA recipient
-    /// @param zoraReward ZORA amount
-    function depositRewards(
-        address creator,
-        uint256 creatorReward,
-        address createReferral,
-        uint256 createReferralReward,
-        address mintReferral,
-        uint256 mintReferralReward,
-        address firstMinter,
-        uint256 firstMinterReward,
-        address zora,
-        uint256 zoraReward
-    ) external payable {
-        if (msg.value != (creatorReward + createReferralReward + mintReferralReward + firstMinterReward + zoraReward)) {
-            revert INVALID_DEPOSIT();
-        }
-
-        unchecked {
-            if (creator != address(0)) {
-                balanceOf[creator] += creatorReward;
-            }
-            if (createReferral != address(0)) {
-                balanceOf[createReferral] += createReferralReward;
-            }
-            if (mintReferral != address(0)) {
-                balanceOf[mintReferral] += mintReferralReward;
-            }
-            if (firstMinter != address(0)) {
-                balanceOf[firstMinter] += firstMinterReward;
-            }
-            if (zora != address(0)) {
-                balanceOf[zora] += zoraReward;
-            }
-        }
-
-        emit RewardsDeposit(
-            creator,
-            createReferral,
-            mintReferral,
-            firstMinter,
-            zora,
-            msg.sender,
-            creatorReward,
-            createReferralReward,
-            mintReferralReward,
-            firstMinterReward,
-            zoraReward
-        );
     }
 
     /// @notice Withdraw protocol rewards
@@ -163,7 +126,7 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
             amount = balanceOf[owner];
         }
 
-        balanceOf[owner] -= amount;
+        _decreaseBalance(owner, amount);
 
         emit Withdraw(owner, to, amount);
 
@@ -240,7 +203,7 @@ contract ProtocolRewards is Enjoy, IProtocolRewards, EIP712 {
             amount = balanceOf[from];
         }
 
-        balanceOf[from] -= amount;
+        _decreaseBalance(from, amount);
 
         emit Withdraw(from, to, amount);
 
